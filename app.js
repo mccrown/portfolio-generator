0afe90683a8353f9221ce39eef741654a9f8fd89
CMcCown <connermccown94@gmail.com>
2022-03-16 09:37:21 -0600

    added inquirer package and code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name,github);
 
 // console.log(profileDataArgs);
 
@@ -23,8 +23,18 @@ const [name, github] = profileDataArgs;
 // printProfileData(profileDataArgs);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
